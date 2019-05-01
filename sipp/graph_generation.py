4605de609089e548102272c3851987b89f4301eb
atb033 <ashwinbose123@gmail.com>
2019-05-01 12:18:51 +0200

    grid class with split interval

--- sipp/graph_generation.py
+++ sipp/graph_generation.py
@@ -10,7 +10,27 @@ See the article: 10.1109/ICRA.2011.5980306
 
 import argparse
 import yaml
+from bisect import bisect
 
+class SippGrid(object):
+    def __init__(self):
+        self.position = ()
+        self.interval_list = [(0, float('inf'))]
+    def split_interval(self, t):
+        for interval in self.interval_list:
+            if t == interval[0]:
+                self.interval_list.remove(interval)
+                if t+1 <= interval[1]:
+                    self.interval_list.append((t+1, interval[1]))
+            elif t == interval[1]:
+                self.interval_list.remove(interval)
+                if t-1 <= interval[0]:
+                    self.interval_list.append((interval[0],t-1))
+            elif bisect(interval,t) == 1:
+                self.interval_list.remove(interval)
+                self.interval_list.append((interval[0], t-1))
+                self.interval_list.append((t+1, interval[1]))
+            self.interval_list.sort()
 
 class SippGraph(object):
     def __init__(self, map):
@@ -28,4 +48,10 @@ if __name__ == "__main__":
         except yaml.YAMLError as exc:
             print(exc)
 
-    sipp_graph = SippGraph(map)    
+    grid = SippGrid()
+    print (grid.interval_list)
+    grid.split_interval(2)    
+    print (grid.interval_list)
+    grid.split_interval(3)    
+    print (grid.interval_list)
+    
